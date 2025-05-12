484c475b10991bc7220bb5ce992535246529df35
SuleymanCanTugcu <TugcuSuleymanCan@gmail.com>
2025-05-12 16:11:10 +0300

    İlk commit: Kuyruk özelliği eklendi

--- obj/Release/net6.0/CallCenterQueueSimulation.AssemblyInfo.cs
+++ obj/Release/net6.0/CallCenterQueueSimulation.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CallCenterQueueSimulation")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a29aefff6bc1f917ac070921268a2f7363ae540")]
 [assembly: System.Reflection.AssemblyProductAttribute("CallCenterQueueSimulation")]
